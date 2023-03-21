a31e24c01b30d892ff83a465e335dcc795e799ea
akhilkirantest <akhilkirank@gmail.com>
2023-03-21 15:44:41 +0530

    Bootsrtap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home.js';
 import About from './Pages/About.js';
 import Contact from './Pages/Contact.js';
